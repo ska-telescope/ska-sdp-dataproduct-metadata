319e53cef4248586ab06dc161905a2f013662ebf
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2023-02-14 13:13:17 +0000

    REL-469: bumped version to 0.1.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -50,9 +50,9 @@ author = "SKA SDP Developers"
 # built documents.
 #
 # The short X.Y.Z version.
-version = "0.1.0"
+version = "0.1.1"
 # The full version, including alpha/beta/rc pre-release tags.
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ------------------------------------------------
 
