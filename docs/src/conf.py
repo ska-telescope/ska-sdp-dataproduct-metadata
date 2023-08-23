bbddda714763aac818bb3acff909ce271f97e76a
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2023-08-23 07:27:29 +0100

    REL-875: bumped version to 0.2.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -51,9 +51,9 @@ author = "SKA SDP Developers"
 # built documents.
 #
 # The short X.Y.Z version.
-version = "0.1.2"
+version = "0.2.0"
 # The full version, including alpha/beta/rc pre-release tags.
-release = "0.1.2"
+release = "0.2.0"
 
 # -- General configuration ------------------------------------------------
 
